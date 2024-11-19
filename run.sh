94c8a9b85bf47f51c32ca4d92280afcf6297fe41
varikoz272 <vornickon@gmail.com>
2024-11-19 18:51:42 +0300

    added lib32s

--- run.sh
+++
@@ -1,9 +0,0 @@
-﻿sudo pacman -S git neovim zig openshot discord telegram-desktop gimp blender audacity fzf obs-studio soundconverter jdk21-openjdk jre21-openjdk dotnet-runtime dotnet-sdk lua vlc squeak-vm p7zip gradle maven libreoffice-still-ru libreoffice-still firefox steam tmux less man cargo go ghc docker zip kdeconnect strace julia prismlauncher neofetch unzip python python-pip yarn postgresql npm vulkan-icd-loader vulkan-tools pulsemixer ttf-font-awesome lldb yazi pulsemixer ttf-font-awesome pavucontrol libntp gufs-mtp glm
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ~
-yay
-yay -S scrcpy a2ln android_ndk heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-
---
+++ yay.sh
@@ -0,0 +1,1 @@
+yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0
