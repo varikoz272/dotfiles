2067a47910e79b5795c5ccd6612518983391ad4b
varikoz272 <vornickon@gmail.com>
2024-12-22 04:27:03 +0300

    added waydroid setup

--- yay.sh
+++ yay.sh
@@ -1,2 +1,7 @@
 yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid
 sudo systemctl enable --now waydroid-container
+cd ~
+git clone https://github.com/choff/anbox-modules.git
+cd anbox-modules
+./INSTALL
+cd ~
