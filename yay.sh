99a91191b01cae0efbfaa42ff4fbcb5525e96abd
varikoz272 <vornickon@gmail.com>
2024-12-22 03:34:06 +0300

    added waydroid

--- yay.sh
+++ yay.sh
@@ -1,1 +1,2 @@
-yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman
+yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid
+sudo systemctl enable --now waydroid-container
