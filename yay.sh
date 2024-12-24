79431a5d12633cf3e0dbc881547cd63b0f04ec07
varikoz272 <vornickon@gmail.com>
2024-12-24 21:31:37 +0300

    added pgadmin4

--- yay.sh
+++ yay.sh
@@ -1,4 +1,4 @@
-yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid
+yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid pgadmin4-server pgadmin4-desktop
 sudo systemctl enable --now waydroid-container
 cd ~
 git clone https://github.com/choff/anbox-modules.git
