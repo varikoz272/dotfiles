f70d4cb0b1dda34d9df7c06d8a7ef6cddd3235fd
varikoz272 <vornickon@gmail.com>
2025-02-03 16:45:35 +0300

    added pgModeler

--- yay.sh
+++ yay.sh
@@ -1,4 +1,4 @@
-yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid pgadmin4-server pgadmin4-desktop rider unity amneziawg-dkms amneziawg-go amneziawg-linux amneziawg-tools anroid-ndk android-studio doom doom1-wad dbeaver-git
+yay -S scrcpy a2ln heroic-games-launcher itch-bin otf-comicshanns-nerd sdl2 clutter glew haguichi spoofdpi hyprpaper waybar hyprlock wofi openssl-1.0 hyprshot lib32-openssl-1.0 ttf-times-new-roman waydroid pgadmin4-server pgadmin4-desktop rider unity amneziawg-dkms amneziawg-go amneziawg-linux amneziawg-tools anroid-ndk android-studio doom doom1-wad dbeaver-git pgModeler
 sudo systemctl enable --now waydroid-container
 cd ~
 git clone https://github.com/choff/anbox-modules.git
